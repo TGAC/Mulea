130aada754cce936a4062141fbe3e4f6bd2455e9
Cezary Turek <cezary.turek@gmail.com>
2016-12-18 13:48:39 +0100

    Improvement of performance of the methods.

--- R/Methods.R
+++ R/Methods.R
@@ -1,18 +1,20 @@
 calculateHypergeometricTest <- function(model, sampleVector) {
     allElements <- unique(unlist(model$listOfValues))
     ddply(.data = model,  .variables = c("category"), .fun = function(dfRow) {
+        modelSampleIntersection <- intersect(dfRow[1, 'listOfValues'][[1]], sampleVector)
+        q <- length(modelSampleIntersection)
+        m <- length(dfRow[1, 'listOfValues'][[1]])
+        n <- length(setdiff(allElements, dfRow[1, 'listOfValues'][[1]]))
+        k <- length(sampleVector)
         data <- data.frame(
             'description' = dfRow['description'],
             'listOfValues' = dfRow["listOfValues"],
-            'category listOfValues' = I(list(intersect(dfRow[1, 'listOfValues'][[1]], sampleVector))),
-            'q' = length(intersect(dfRow[1, 'listOfValues'][[1]], sampleVector)),
-            'm' = length(dfRow[1, 'listOfValues'][[1]]),
-            'n' = length(setdiff(allElements, dfRow[1, 'listOfValues'][[1]])),
-            'k' = length(sampleVector),
-            'p-value' = phyper(length(intersect(dfRow[1, 'listOfValues'][[1]], sampleVector)),
-                               length(dfRow[1, 'listOfValues'][[1]]),
-                               length(setdiff(allElements, dfRow[1, 'listOfValues'][[1]])),
-                               length(sampleVector), lower.tail = FALSE, log.p = FALSE))
+            'listOfValuesUnderCategory' = I(list(modelSampleIntersection)),
+            'q' = q,
+            'm' = m,
+            'n' = n,
+            'k' = k,
+            'p-value' = phyper(q, m, n, k, lower.tail = FALSE, log.p = FALSE))
     })
 }
 
@@ -20,19 +22,20 @@ calculateTestOnContingencyTable <- function(testMethod, testResultsColumnName) {
   function(model, sampleVector) {
     allElements <- unique(unlist(model$listOfValues))
     ddply(.data = model,  .variables = c("category"), .fun = function(dfRow) {
+      modelSampleIntersection <- intersect(dfRow[1, 'listOfValues'][[1]], sampleVector)
+      q <- length(modelSampleIntersection)
+      m <- length(dfRow[1, 'listOfValues'][[1]])
+      n <- length(setdiff(allElements, dfRow[1, 'listOfValues'][[1]]))
+      k <- length(setdiff(sampleVector, modelSampleIntersection))
       data <- data.frame(
         'description' = dfRow['description'],
         'listOfValues' = dfRow["listOfValues"],
-        'category listOfValues' = I(list(intersect(dfRow[1, 'listOfValues'][[1]], sampleVector))),
-        'q' = length(intersect(dfRow[1, 'listOfValues'][[1]], sampleVector)),
-        'm' = length(dfRow[1, 'listOfValues'][[1]]),
-        'n' = length(setdiff(allElements, dfRow[1, 'listOfValues'][[1]])),
-        'k' = length(setdiff(sampleVector, intersect(dfRow[1, 'listOfValues'][[1]], sampleVector))),
-        testResultsColumnName = I(list(testMethod(matrix(c(length(dfRow[1, 'listOfValues'][[1]]),
-                                                         length(intersect(dfRow[1, 'listOfValues'][[1]], sampleVector)),
-                                                         length(setdiff(allElements, dfRow[1, 'listOfValues'][[1]])),
-                                                         length(setdiff(sampleVector, intersect(dfRow[1, 'listOfValues'][[1]], sampleVector)))),
-                                                       nrow = 2)))))
+        'listOfValuesUnderCategory' = I(list(modelSampleIntersection)),
+        'q' = q,
+        'm' = m,
+        'n' = n,
+        'k' = k,
+        testResultsColumnName = I(list(testMethod(matrix(c(m, q, n, k), nrow = 2)))))
     })
   }
 }
