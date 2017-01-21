3d0d10438c6679091fefb7866f8b7384de039beb
Cezary Turek <cezary.turek@gmail.com>
2017-01-21 18:53:48 +0100

    Part of work on GSEA perm test.

--- workdir/work.R
+++ workdir/work.R
@@ -1,60 +1,37 @@
-res[0]
-res[1]
-res[2]
-res[3]
-typeof(res[3])
-res[3][2]
-X <- read.table(textConnection(res[3]))
-Y <- unlist(X)
-names(Y) <- NULL
-typeof(Y)
-X[2]
 
-REACTOME_database_dmel_uniq.txt
-rt <- read.table(file = "/home/koralgooll/doktorat/Rpackages/mulea/Mulea/example/REACTOME_database_dmel_uniq.txt", header = FALSE, fill = TRUE)
-head(rt)
-rt[1,]
-
-
-db <- DBI::dbConnect(RSQLite::SQLite(), dbname = "./Mulea.sqlite")
-
-DBI::dbSendQuery(conn = db, "INSERT INTO organisms VALUES (9606,'homo sapiens');")
-DBI::dbSendQuery(conn = db, "INSERT INTO organisms VALUES (3006,'mouse');")
-DBI::dbSendQuery(conn = db, "INSERT INTO organisms VALUES (3130,'muszka');")
-
-DBI::dbSendQuery(conn = db, "ANALYZE;")
+aa <- data.frame(hTestResults, "lalala" = c("1","2","3","4","5","6","7"))
 
 
 
-# Load your R dataframe
-load(file = "~/RProject/sqlite/DATA/Class.rda")
+R_value_obs <- integer(0)
+for (i in 1:length(hTestResults$p.value)) {
+  R_value_obs[i] <- sum(hTestResults$p.value <= hTestResults$p.value[i])
+}
 
-l <- list(taxonomy_id = 2132, latin_name="roxi pospolita")
-organism <- data.frame(l)
-DBI::dbSendQuery(conn = db, paste("INSERT INTO organisms VALUES (", organism$taxonomy_id, ",'", organism$latin_name, "');"))
-DBI::dbListFields(db, "organisms")
-DBI::dbListTables(db)
-DBI::dbWriteTable(conn = db, name = "new_new_organisms", value = organism, row.names = TRUE)
-DBI::dbListFields(db, "new_new_organisms")
-
-# Push to SQLite
-DBI::dbWriteTable(conn = db, name = "organisms", value = organism, row.names = FALSE, append = TRUE)
+allElements <- unique(unlist(hTestResults$listOfValues))
+simulationMatrix <- array(dim = c(length(hTestResults$p.value), steps))
+for (j in 1:steps) {
+  randomData <- sample(allElements, length(dataFromExperiment))
+  for (i in 1:length(hTestResults$p.value)) {
 
 
+    modelSampleIntersection <- intersect(hTestResults[i, 'listOfValues'][[1]], randomData)
+    q <- length(modelSampleIntersection)
+    m <- length(hTestResults[i, 'listOfValues'][[1]])
+    n <- length(setdiff(allElements, hTestResults[i, 'listOfValues'][[1]]))
+    k <- length(randomData)
 
+    # Why 1 - pvalue from test?
+    simulationMatrix[i,j] = 1 - phyper(q, m, n, k, lower.tail = FALSE, log.p = FALSE)
+  }
+}
+simulationVector <- as.vector(simulationMatrix)
+R_value_exp <- integer(0)
+for (l in 1:length(hTestResults$p.value)) {
+  # Why is 15 digits?
+  # P_Sim_round=round(P_Sim_vec, digits=15)
+  R_value_exp[l] <- sum(simulationVector <= hTestResults$p.value[l]) / steps
+}
 
-DBI::dbExistsTable(conn = db, name = "orgisms")
+finalDf <- data.frame(hTestResults, "EszterAlg" = R_value_exp / R_value_obs)
 
-result <- DBI::dbListResults(db)[[1]]
-DBI::fetch(res = result)
-
-results <- DBI::dbListResults(db)
-DBI::fetch(res = results)
-DBI::dbDisconnect(conn = db)
-
-
-n = c(2, 3, 5)
-s = c("aa", "bb", "cc")
-b = c(TRUE, FALSE, TRUE)
-df = data.frame(n, s, b)
-df
