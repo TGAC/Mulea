7d6c8b2674e8f34ea0c5e11be6fe1553cdb2c628
Cezary Turek <cezary.turek@gmail.com>
2017-01-23 11:56:23 +0000

    Directory's restructuration part 2.

--- R/Mulea.R
+++
@@ -1,6 +0,0 @@
-
-.onAttach <- function(libname, pkgname) {
-    packageStartupMessage("Welcome to MulEA package!")
-    assign("databaseConnection", NULL, envir = .GlobalEnv)
-    assign("databaseLocalization", NULL, envir = .GlobalEnv)
-}
