e56ed68212704a90c0e1d84bfd6ebc015e6951d8
Cezary Turek <cezary.turek@gmail.com>
2017-01-24 19:26:27 +0000

    Benchmark test foor permutation test

--- Rpackage/workdir/work.R
+++ Rpackage/workdir/work.R
@@ -1,8 +1,66 @@
-modelDfFromFile <- MulEA::readGmtFileAsDF(gmtFilePath = "/home/koralgooll/doktorat/Rpackages/Mulea/example/GO2allGenes_FBgnIDs_v2.gmt")
+cat("\014")
+modelDfFromFile <- MulEA::readGmtFileAsDF(gmtFilePath = "/home/koralgooll/doktorat/Rpackages/Mulea/Rpackage/example/GO2allGenes_FBgnIDs_v2.gmt")
+modelDfFromFile <- MulEA::readGmtFileAsDF(gmtFilePath = "/home/koralgooll/doktorat/Rpackages/Mulea/Rpackage/inst/example/model.gmt")
 steps <- 2
 sampleVector <- sample(unique(unlist(modelDfFromFile$listOfValues)), 200)
 modelWithTestDf <- MulEA::calculateHypergeometricTest(model = modelDfFromFile, sampleVector = sampleVector)
 length(unique(unlist(modelWithTestDf$listOfValues)))
+
+a2Z <- function() {
+  allElements <- unique(unlist(modelWithTestDf$listOfValues))
+  dataMatrix <- array(dim = c(length(modelWithTestDf$category), length(allElements)))
+  for (i in 1:length(modelWithTestDf$category)) {
+    concatenation <- c(modelWithTestDf[i,]$listOfValues[[1]], allElements)
+    dataMatrix[i,] <- duplicated(concatenation)[(length(modelWithTestDf[i,]$listOfValues[[1]])+1):length(concatenation)]
+  }
+  simulationMatrix <- array(dim = c(length(modelWithTestDf$p.value), steps))
+  for (j in 1:steps) {
+    randomData <- logical(length(allElements))
+    randomData[sample(length(allElements), size = length(sampleVector))] <- TRUE
+    for (i in 1:length(modelWithTestDf$p.value)) {
+      modelSampleIntersection <- dataMatrix[i,] & randomData
+      q <- sum(modelSampleIntersection)
+      m <- length(modelWithTestDf[i, 'listOfValues'][[1]])
+      n <- length(allElements) - sum(dataMatrix[i,])
+      k <- sum(randomData)
+      # Why 1 - pvalue from test?
+      simulationMatrix[i,j] <- 1 - phyper(q, m, n, k, lower.tail = FALSE, log.p = FALSE)
+    }
+  }
+  simulationVector <- as.vector(simulationMatrix)
+  simulationVector
+}
+
+modelSampleIntersection <- dataMatrix[1,] & randomData
+q <- sum(modelSampleIntersection)
+m <- length(modelWithTestDf[1, 'listOfValues'][[1]])
+n <- length(allElements) - sum(dataMatrix[1,])
+k <- sum(randomData)
+# Why 1 - pvalue from test?
+simulationMatrix[i,j] <- 1 - phyper(q, m, n, k, lower.tail = FALSE, log.p = FALSE)
+
+
+aaa <- array(dim = c(10,3))
+for (i in 1:10) {
+  newOne <- array(dim = c(1,3))
+  newOne[1, 1:3] <- FALSE
+  newOne[1, sample(3,1)] <- TRUE
+  aaa[i,] <- newOne
+}
+
+aaa[9,] & newOne
+
+randomData[c(3,5,7)] <- TRUE
+a2Xdata[1,3:10] <- TRUE
+sum(randomData)
+sum(a2Xdata[1,])
+sum(a2Xdata[1,] & randomData)
+
+
+dupa <- logical(length(allElements))
+dupa[sample.int(length(allElements), size = length(sampleVector))] <- TRUE
+sum(dupa)
+dataMatrix[1:20, 1:10]
 
 
 a2X <- function() {
@@ -49,6 +107,8 @@ a2Y <- function() {
 }
 
 
+
+
 b2 <- function() {
   allElements <- unique(unlist(modelWithTestDf$listOfValues))
   k <- 1
@@ -71,15 +131,33 @@ b2 <- function() {
   simulationVector
 }
 
-
-for (i in c(10, 100, 200)) {
-  for (j in c(20, 200, 500)) {
+cat("\014")
+for (i in c(10, 20, 30)) {
+  for (j in c(200)) {
     steps <- i
     sampleVector <- sample(unique(unlist(modelDfFromFile$listOfValues)), j)
-    system.time(a2Xdata <- a2X())
-    system.time(a2Ydata <- a2Y())
+    print("**********************************")
+    print(paste(i, j, sep = " - "))
+    print(system.time(a2Xdata <- MulEA::gseaPermutationTest(modelWithTestDf, steps, sampleVector)))
+    print(system.time(a2Ydata <- MulEA::gseaPermutationTestPlyr(modelWithTestDf, steps, sampleVector)))
+    print(system.time(a2Zdata <- MulEA::gseaPermutationTestWithBinaryMatrix(modelWithTestDf, steps, sampleVector)))
+    print(a2Xdata[1:10])
+    print(a2Ydata[1:10])
+    print(a2Zdata[1:10])
   }
 }
-system.time(a2Xdata <- a2X())
+
+steps <- 10
+
+system.time(a2Zdata <- a2Z())
+
 system.time(a2Ydata <- a2Y())
+
+
+a2Xdata[1:20,1:10]
+
+
 sum(a2Xdata == a2Ydata)
+
+
+round(c(123.456, 123.4111156, 123.453336, 123.4563),digits=2)
